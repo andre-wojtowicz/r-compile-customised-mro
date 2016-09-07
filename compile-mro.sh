d3f80c8fdee4f10952839ab0bcfd371442eb059e
Andrzej Wójtowicz <andre-wojtowicz@users.noreply.github.com>
2016-09-07 16:39:52 +0200

    Update compile-mro.sh

--- compile-mro.sh
+++ compile-mro.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Debian
 
 R_VERSION="3.3.1"
@@ -7,9 +6,9 @@ CHECKPOINT_SNAPSHOT_DATE="2016-07-01"
 NEW_CONNECTION_LIMIT="2048"
 DEST_DIR="/usr/lib64/microsoft-r/${R_VERSION:0:3}"
 
-sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade
-sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install \
+apt-get update
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade
+apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install \
     build-essential gfortran libreadline-dev libx11-dev libxt-dev zlib1g-dev libbz2-dev liblzma-dev \
     libpcre3-dev libcurl4-openssl-dev openjdk-7-jdk openjdk-7-jre
 
@@ -27,27 +26,27 @@ cd build
 
 ../configure LIBnn=lib64 --enable-R-shlib
 make -j `nproc`
-sudo make prefix=${DEST_DIR} install
-sudo rm -r ${DEST_DIR}/bin
+make prefix=${DEST_DIR} install
+rm -r ${DEST_DIR}/bin
 
-sudo ln -s ${DEST_DIR}/lib64/R/bin/R /usr/bin/R
-sudo ln -s ${DEST_DIR}/lib64/R/bin/Rscript /usr/bin/Rscript
+ln -s ${DEST_DIR}/lib64/R/bin/R /usr/bin/R
+ln -s ${DEST_DIR}/lib64/R/bin/Rscript /usr/bin/Rscript
 
 cd ../../additionalPackages
-sudo Rscript -e "install.packages(c('foreach', 'iterators', 'RUnit', 'checkpoint'), repos='http://mran.microsoft.com/snapshot/${CHECKPOINT_SNAPSHOT_DATE}')"
-sudo R CMD INSTALL -l ${DEST_DIR}/lib64/R/library/ doParallel RevoIOQ RevoMods RevoUtils
+Rscript -e "install.packages(c('foreach', 'iterators', 'RUnit', 'checkpoint'), repos='http://mran.microsoft.com/snapshot/${CHECKPOINT_SNAPSHOT_DATE}')"
+R CMD INSTALL -l ${DEST_DIR}/lib64/R/library/ doParallel RevoIOQ RevoMods RevoUtils
 
 cp ../../../Rprofile.site /${DEST_DIR}/lib64/R/etc/Rprofile.site
 
-sudo R CMD javareconf # additional Java reconfiguration might be helpful
+R CMD javareconf # additional Java reconfiguration might be helpful
 
 # Compress files:
 #   tar -cvf mro-${R_VERSION}.tar.gz ${DEST_DIR}
 
 # Extract files:
-#   sudo tar -xvf mro-${R_VERSION}.tar.gz -C /
+#   tar -xvf mro-${R_VERSION}.tar.gz -C /
 
 # Uninstall R:
-#   sudo rm -r ${DEST_DIR}
-#   sudo rm /usr/bin/R
-#   sudo rm /usr/bin/Rscript
+#   rm -r ${DEST_DIR}
+#   rm /usr/bin/R
+#   rm /usr/bin/Rscript
